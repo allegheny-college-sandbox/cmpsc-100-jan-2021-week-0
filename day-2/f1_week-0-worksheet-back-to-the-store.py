6f9cd8aa10b20e2f06837013617911c90a4ba2d2
dluman <dluman@allegheny.edu>
2021-01-17 23:23:03 -0500

    ALL THE WRITING

---
+++ day-2/f1_week-0-worksheet-back-to-the-store.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+shopping_list = [
+    "screwdriver", 
+    "wire", 
+    "wall plug",
+    "spark plug", 
+    "candy bar"
+]
+
+store_inventory = {
+    "screwdriver": {
+        "quantity": 20, 
+        "price": 2.25
+    },
+    "wire": {
+        "quantity": 12, 
+        "price": .79
+    },
+    "wall plug": {
+        "quantity": 10, 
+        "price": 1.00
+    },
+    "spark plug": {
+        "quantity": 49, 
+        "price": .50
+    },
+    "candy bar": {
+        "quantity": 12, 
+        "price": .99
+    }
+}
+
+tax = .08
+
+# TODO: Finish the task according to the accompanying worksheet
